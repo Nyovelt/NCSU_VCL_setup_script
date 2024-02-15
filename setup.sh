003106321b167a4d8c2de88efb4fe441b767b67f
nyovelt <canarypwn@aosc.io>
2024-02-15 14:25:05 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo sed -i 's@//.*archive.ubuntu.com@//atl.mirrors.clouvider.net@g' /etc/apt/so
 # Update packages
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y btop neofetch tmux screen htop python3-pip llvm docker.io
+sudo apt-get install -y btop neofetch tmux screen htop python3-pip llvm docker.io docker-compose
 
 
 
